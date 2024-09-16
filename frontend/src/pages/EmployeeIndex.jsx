46011c4dbb7f6e29c0796b6bff58446e5062fe8f
JennyLottner <jenny.lottner@gmail.com>
2024-09-16 23:35:50 +0300

    Add index page filter and styling

--- frontend/src/pages/EmployeeIndex.jsx
+++ frontend/src/pages/EmployeeIndex.jsx
@@ -1,5 +1,37 @@
+import { useEffect, useMemo, useState } from 'react'
+
+import { employeeService } from '../services/employee.local.service'
+import { utilService } from '../services/util.service'
+
 export function EmployeeIndex() {
+    const [employees, setEmployees] = useState([])
+    const [filterTxt, setFilter] = useState('')
+
+    useEffect(() => {
+        async function loadEmployees() {
+            try {
+                const data = await employeeService.query(filterTxt)
+                setEmployees(data)
+            } catch (err) {
+                console.log(err)
+            }
+        }
+        loadEmployees()
+    }, [filterTxt])
+
+    function onFilter({ target }) {
+        debouncedSetFilter(target.value)
+    }
+    const debouncedSetFilter = useMemo(() =>
+        utilService.debounce((value) => {
+            setFilter(value)
+        }, 300), [])
+
     return (
-        <h1>Employee Directory</h1>
-    )
+        <section className='search-page flex column center'>
+            <h1>Looking for an employee?</h1>
+            <p>Click on the search bar to learn our suggestions</p>
+            <input type="text"  placeholder='Search...' onInput={onFilter}/>
+        </section>
+    );
 }
