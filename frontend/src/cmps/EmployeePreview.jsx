aa4bbba556bf473d044ad4fad41e12c4db246dc2
JennyLottner <jenny.lottner@gmail.com>
2024-09-17 01:33:05 +0300

    Add image to employee preview

--- frontend/src/cmps/EmployeePreview.jsx
+++ frontend/src/cmps/EmployeePreview.jsx
@@ -1,8 +1,10 @@
 export function EmployeePreview({ employee }) {
 
     return (
-        <li key={employee._id}>
-            <strong>{employee.name}</strong> - {employee.workTitle}
+        <li key={employee._id} className="employee-preview grid">
+            <img src={employee.imgUrl} alt={employee.name} />
+            <strong>{employee.name}</strong>
+            <p>{employee.workTitle}</p>
         </li>
     )
 }
