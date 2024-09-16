bd2912537b134b7f488c2d9f08226cfadc435c28
JennyLottner <jenny.lottner@gmail.com>
2024-09-17 01:33:35 +0300

    Add employee list and preview styling

--- frontend/src/cmps/EmployeeList.jsx
+++ frontend/src/cmps/EmployeeList.jsx
@@ -3,11 +3,13 @@ import { EmployeePreview } from "./EmployeePreview";
 export function EmployeeList({ employees }) {
 
     return (
-        <ul className="employee-list flex column">
-            {employees && employees.length > 0 && employees.map(employee => (
-                <EmployeePreview employee={employee} />
-            ))}
-            {!employees.length && <li>Try using more general terms to broaden your search.</li>}
-        </ul>
+        <section className="employee-list-section">
+            <ul className="employee-list flex column">
+                {employees && employees.length > 0 && employees.map(employee => (
+                    <EmployeePreview employee={employee} />
+                ))}
+                {!employees.length && <li className="no-results">Try using more general terms to broaden your search.</li>}
+            </ul>
+        </section>
     )
 }
